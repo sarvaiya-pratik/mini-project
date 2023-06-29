9463d46cab1cba3cac53af810bc7953915e72e60
sarvaiya-pratik <pratikrajput125@gmail.com>
2023-06-29 20:17:50 +0530

    update

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -3,6 +3,8 @@ import "./app.css"
 import Header from "./common/Header/Header"
 import Footer from "./common/Footer/Footer"
 import Home from "./pages/home/Home"
+import About from "./pages/about/About"
+import Fature from "./pages/fature/Fature"
 import Login from "./pages/login/Login"
 import Signup from "./pages/sign-up/Signup"
 function App() {
@@ -14,6 +16,8 @@ function App() {
     <Header/>
         <Routes>
           <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+          <Route path="/fature" element={<Fature/>}></Route>
           <Route path="/login" element={<Login/>}></Route>
           <Route path="/signup" element={<Signup/>}></Route>
 
