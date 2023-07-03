8d753996cafe5345a38a1089f7b8f30ca0559cdd
sarvaiya-pratik <pratikrajput125@gmail.com>
2023-07-03 18:15:41 +0530

    hii

--- Server/index.js
+++ Server/index.js
@@ -3,12 +3,19 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const path = require('path')
 
 const URLATLAS = "mongodb+srv://sarvaiya:pratik@first-project.52mrdfn.mongodb.net/HelloDB"
 const app = express()
 const PORT = 4001
 app.use(cors())
 app.use(bodyParser.json())
+
+// static file Client\dist\index.html
+app.use(express.static(path.join(__dirname,"./Client/dist")))
+app.get('*',function(req,res){
+    res.sendFile(path.join(__dirname,"./Client/dist/index.html"))
+})
 // mongo connect
 const StudModel = require("./models/Student")
 mongoose.connect(URLATLAS);
